844fd210b137c0f610c155f8ec1bd4086d0bebe2
julianopaulos <julianopaulo.santos@hotmail.com>
2022-05-13 07:13:24 -0300

    alterações

--- lista linear sequencial/index.cpp
+++ lista linear sequencial/index.cpp
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include <malloc.h>
 
 #define MAX 50
 
@@ -18,6 +19,7 @@ typedef struct{
 //gerada uma nova lista (cópia) com o novo dado passado,
 // ao invés de alterar uma já existente
 void initList(LIST* list){
+	printf("list %i", list->numberElements);
 	list->numberElements = 0;
 }
 
@@ -26,11 +28,12 @@ int getNumberElements(LIST* list){
 }
 
 void showList(LIST* list){
+	printf("teste");
 	int i;
 	printf("Lista: \"");
 	for(i=0; i < list->numberElements; i++){
 		//mostra o registro e a sua chave
-		printf("%i", list[i]->A.key);
+		printf("%i", list->A[i].key);
 	}
 	printf("\"\n");
 }
@@ -50,12 +53,13 @@ int searchListElement(LIST* list, KEYTYPE key){
 
 bool insertListElement(LIST* list, REGISTER reg, int i){
 	int j;
-	
+	printf("tes");
 	//lista cheia
 	if(list->numberElements == MAX || (i < 0) || (i > list->numberElements)){
+		printf("gergrfg");
 		return false;
 	}
-	
+	printf("teast");
 	//desloca todos os registros da lista para uma posição a direita
 	//para poder inserir o novo na primeira posição
 	for(j = list->numberElements; j > i; j--){
@@ -71,7 +75,41 @@ bool insertListElement(LIST* list, REGISTER reg, int i){
 	
 }
 
+bool deleteListElement(KEYTYPE key, LIST* list){
+	int pos, j;
+	
+	pos = searchListElement(list, key);
+	
+	//não encontrou item na lista
+	if(pos == -1){
+		return false;
+	}
+	
+	for(j = pos; j < list->numberElements-1; j++){
+		list->A[j] = list->A[j+1];
+	}
+	
+	list->numberElements--;
+	return true;
+	
+}
+
+void resetList(LIST* list){
+	list->numberElements = 0;
+}
+
 int main(){
 	//para inicializar uma lista, só precisamos colocar o valor0
 	//no número de elementos válidos
+	
+	LIST* list;
+	//printf("\ntest %i", getNumberElements(list));
+	REGISTER reg;
+	reg.key = 0;
+	initList(list);
+	printf("test");
+	insertListElement(list, reg, 1);
+	
+	showList(list);
+	
 }
