c95aa81cd2a9df56aa74b3726d0fc1191e29447f
julianopaulos <julianopaulo.santos@hotmail.com>
2022-05-20 12:33:46 -0300

    gitignore e arrray

---
+++ arrays/por rand.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+	int i;
+	
+	int numbers[10];
+	
+	char letters[100];
+	
+	//rand seed for number generate
+	// (if seed is always the same, the numbers will be always
+	//the same too)
+	//srand(150);
+	
+	srand(time(NULL));
+	
+	for(int i = 0; i < sizeof(numbers)/sizeof(numbers[0]); i++){
+		numbers[i] = rand();
+	}
+	
+	printf("\n");
+	for(int i = 0; i < sizeof(numbers)/sizeof(numbers[0]); i++){
+		printf("Elemento da posicao %i: %i\n", i, numbers[i]);
+	}
+	
+	return 0;
+}
