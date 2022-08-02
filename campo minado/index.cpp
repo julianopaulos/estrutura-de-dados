c1a0c36419ca460d355654d7b63c1507fc6fee03
julianopaulos <julianopaulo.santos@hotmail.com>
2022-08-02 07:01:53 -0300

    tentando definir posição das bombas

--- campo minado/index.cpp
+++ campo minado/index.cpp
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<time.h>
 
 int main(){
-	int table[10][10], line, column;
+	int table[10][10], line, column, bombs[3][3];
 	
 	printf("   0  1  2  3  4  5  6  7  8  9\n");
 	printf("  -------------------------------\n");
@@ -26,6 +27,26 @@ int main(){
 	printf("  -------------------------------\n");
 	printf("9 |  |  |  |  |  |  |  |  |  |  |\n");
 	printf("  -------------------------------\n");
+	
+	srand(time(NULL));
+	
+	for(int i = 0; i < sizeof(bombs)/sizeof(bombs[0]); i++){
+		for(int j = 0; j < sizeof(bombs[i])/sizeof(bombs[0][0]); j++){
+			bombs[i][j] = rand() % 10;
+		}
+	}
+	
+	for(int i = 0; i < sizeof(bombs)/sizeof(bombs[0]); i++){
+		for(int j = 0; j < sizeof(bombs[i])/sizeof(bombs[0][0]); j++){
+			printf("bomb position: %i\n", bombs[i][j]);
+		}
+	}
+	
+	for(int i = 0; i < sizeof(table)/sizeof(table[0]); i++){
+		for(int j = 0; i < sizeof(table[i])/sizeof(table[0][0]); i++){
+			
+		}
+	}
 	
 	printf("\nDigite as coordenadas: ");
 	scanf("%i%i", &line, &column);
